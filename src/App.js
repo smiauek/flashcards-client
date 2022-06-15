3a05141b9b52ab6ddeeddb60fbb9ae8c3103d795
Piotr Smialkowski <smiaucius@gmail.com>
2022-06-15 11:58:49 -0500

    editing cards now works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Browse from "./components/browse/Browse";
 import AddCard from "./components/card/AddCard";
+import EditCard from "./components/card/EditCard";
 import CreateDeck from "./components/deck/CreateDeck";
 import Deck from "./components/deck/Deck";
 import Header from "./components/layout/Header";
@@ -26,6 +27,10 @@ function App() {
             />
             <Route path="/decks/:deckId/study" element={<Study />} />
             <Route path="/decks/:deckId/cards/new" element={<AddCard />} />
+            <Route
+              path="/decks/:deckId/cards/:cardId/edit"
+              element={<EditCard />}
+            />
             <Route path="/decks/new" element={<CreateDeck />} />
             <Route path="/browse/deck/:deckId" element={<Deck />} />
             <Route path="/browse" element={<Browse />} />
