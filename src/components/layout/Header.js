45b2adbb0c909dbcc9996820a01ea7d79cbd9d4d
Piotr Smialkowski <smiaucius@gmail.com>
2022-06-25 12:39:27 -0500

    adjusted side bar width and header font size

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-dark pb-2 pt-2">
+    <header className="bg-dark pb-2 pt-2" style={{ height: 110 }}>
       <div className="container text-white">
-        <h1 className="text-center">Flashcards</h1>
+        <h1 className="text-center" style={{ fontSize: 60 }}>
+          Flashcards
+        </h1>
       </div>
     </header>
   );
